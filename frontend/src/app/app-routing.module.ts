157c5731dbcce96b6236af7496d5ee62589219dd
MoulinZ <zacharie.moulin@gmail.com>
2023-07-27 13:53:54 +0200

    add home and data components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataComponent } from './components/data/data.component';
+import { HomeContentComponent } from './components/home-content/home-content.component';
 import { LoginComponent } from './home-rnf/components/login/login.component';
 import { LogoutComponent } from './home-rnf/components/logout/logout.component';
 import { NavHomeComponent } from './home-rnf/components/nav-home/nav-home.component';
+import { AuthGuardService } from './home-rnf/services/auth-guard.service';
 import { LazyDialogLoader } from './home-rnf/services/lazy-dialog-loader.service';
 import { LogoutLinkService } from './home-rnf/services/logout-link.service';
 
@@ -11,6 +14,10 @@ const routes: Routes = [
     path: '',
     component: NavHomeComponent,
     children: [
+      {
+        path: '',
+        component: HomeContentComponent
+      },
       {
         path: 'logout',
         // Ici seulement pour angular, mais toujour redirigé dans le canActivate
@@ -21,6 +28,11 @@ const routes: Routes = [
         path: 'login',
         component: LoginComponent,
         canActivate: [ LazyDialogLoader ]
+      },
+      {
+        path: 'explorer',
+        component: DataComponent,
+        canActivate: [ AuthGuardService]
       }
     ]
   }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataComponent } from './components/data/data.component';
+import { HomeContentComponent } from './components/home-content/home-content.component';
 import { HomeRnfModule } from './home-rnf/home-rnf.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeContentComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule,

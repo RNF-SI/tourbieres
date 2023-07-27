8e3d8d97523e391c9798c74e969222b523b1ff3b
MoulinZ <zacharie.moulin@gmail.com>
2023-07-27 10:45:44 +0200

    init app

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './home-rnf/components/login/login.component';
+import { LogoutComponent } from './home-rnf/components/logout/logout.component';
+import { NavHomeComponent } from './home-rnf/components/nav-home/nav-home.component';
+import { LazyDialogLoader } from './home-rnf/services/lazy-dialog-loader.service';
+import { LogoutLinkService } from './home-rnf/services/logout-link.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: NavHomeComponent,
+    children: [
+      {
+        path: 'logout',
+        // Ici seulement pour angular, mais toujour redirigé dans le canActivate
+        component: LogoutComponent,
+        canActivate: [ LogoutLinkService ]
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+        canActivate: [ LazyDialogLoader ]
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/conf/app.config.ts
@@ -0,0 +1,20 @@
+export const AppConfig = {
+    "ID_APPLICATION_GEONATURE": 5,
+    "API_ENDPOINT": "http://127.0.0.1:5088",
+    "appName": "Tourbières RNF",
+    "appTitle": "Tourbières des réserves naturelles",
+    "appSubTitle": "Découvrez les tourbières du réseau",
+    "creditHeaderImage": "Etang du Moulin Neuf, Plounérin - © C. Le Gac",
+    "menu": [
+        {
+            "nom":"accueil", 
+            "classFa":"fas" as const,
+            "nomFa":"house" as const,
+            "lien":""
+        },{
+            "nom":"explorer", 
+            "classFa":"fas" as const,
+            "nomFa":"magnifying-glass" as const,
+            "lien":"explorer"
+        }]
+}
